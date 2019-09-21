1784dbaf169440999b0e91049fca51cce9b73ce1
nathalyvallejo <nathalyvp22@hotmail.com>
2019-09-20 23:55:25 -0500

    Añadiendo Switches

---
+++ ProyectoCEN.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='33.111.44.0/8')
+
+    info( '*** Adding controller\n' )
+    info( '*** Add switches\n')
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch, failMode='standalone')
+    s8 = net.addSwitch('s8', cls=OVSKernelSwitch, failMode='standalone')
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch, failMode='standalone')
+    s7 = net.addSwitch('s7', cls=OVSKernelSwitch, failMode='standalone')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch, failMode='standalone')
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch, failMode='standalone')
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
